d6e41172cb200e25949cdd792301ed2c318751b4
Joshua Ihesiulo <joshuaihesiulo@gmai.com>
2026-03-27 12:26:39 +0100

    Question2

---
+++ Question2.js
@@ -0,0 +1,15 @@
+let car = {
+  brand: "Toyota",
+  model: "Corolla",
+  year: 2019,
+  price: 5500000
+};
+
+console.log(car.brand)
+console.log(car.price)
+
+// changing the price
+
+car.price= 4800000
+car.colour= "white"
+console.log(car)
