c1ba03fc2981fdbd23fced63052a0f1eed7982a7
Joshua Ihesiulo <joshuaihesiulo@gmai.com>
2026-03-28 09:39:45 +0100

    Added function to Question10

--- Question10.js
+++ Question10.js
@@ -1,17 +1,22 @@
-let numericals= [
-    {number: 10},
-        {number: 22},
-        {number: 33},
-        {number: 44},
-        {number: 55
-    }]
-let sumOfEven= 0
-
-
-for(i=0; i<numericals.length; i++){
-    if(numericals[i].number %2===0){
-        sumOfEven+= numericals[i].number
-        console.log(`Even`)
-    } else (console.log(`False`))
+function claculateSumOfEvens(){
+   
+    let numericals= [
+        {number: 10},
+            {number: 22},
+            {number: 33},
+            {number: 44},
+            {number: 55
+        }]
+    let sumOfEven= 0
+    
+    
+    for(i=0; i<numericals.length; i++){
+        if(numericals[i].number %2===0){
+            sumOfEven+= numericals[i].number
+            console.log(`Even`)
+        } else (console.log(`False`))
+    }
+    console.log(sumOfEven)
+    return
 }
-console.log(sumOfEven)
+claculateSumOfEvens()
