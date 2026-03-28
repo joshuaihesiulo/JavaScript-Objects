e1367e4757fa19f5b5f9ead4d20f67f79262a0b5
Joshua Ihesiulo <joshuaihesiulo@gmai.com>
2026-03-28 09:23:30 +0100

    Added function to Question2

--- Question2.js
+++ Question2.js
@@ -1,16 +1,21 @@
-let car = {
-  brand: "Toyota",
-  model: "Corolla",
-  year: 2019,
-  price: 5500000
-};
+function representValues(){
 
-console.log(car.brand)
-console.log(car.price)
-
-// changing the price
-car.price= 4800000
-
-car.colour= "white"
-
-console.log(car)
+  let car = {
+    brand: "Toyota",
+    model: "Corolla",
+    year: 2019,
+    price: 5500000
+  };
+  
+  console.log(car.brand)
+  console.log(car.price)
+  
+  // changing the price
+  car.price= 4800000
+  
+  car.colour= "white"
+  
+  console.log(car)
+  return
+}
+representValues()
