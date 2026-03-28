77bc9a4df9e72544c064cf617ab479a0c5b23fe2
Joshua Ihesiulo <joshuaihesiulo@gmai.com>
2026-03-28 09:34:14 +0100

    Added function to Question7

--- Question7.js
+++ Question7.js
@@ -1,14 +1,19 @@
-let profile= 
-    {firstName: "Joshua",
-    lastName: "Ihesiulo",
-    age: 18,
-    hobbies: ["Playing", "Video", "Games", "and", "reading", "playing", "football"]}
+function listHobbies(){
 
-let fullName= profile.firstName + " " + profile.lastName
-
-console.log(fullName)
-
-for(i=0; i<profile.hobbies.length; i++){
-    console.log(profile.hobbies[i])
+    let profile= 
+        {firstName: "Joshua",
+        lastName: "Ihesiulo",
+        age: 18,
+        hobbies: ["Playing", "Video", "Games", "and", "reading", "playing", "football"]}
     
+    let fullName= profile.firstName + " " + profile.lastName
+    
+    console.log(fullName)
+    
+    for(i=0; i<profile.hobbies.length; i++){
+        console.log(profile.hobbies[i])
+        
+    }
+    return
 }
+listHobbies()
