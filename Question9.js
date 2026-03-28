082d2f8ae5e9be9833de6b38288c14547bbe31e0
Joshua Ihesiulo <joshuaihesiulo@gmai.com>
2026-03-28 09:38:51 +0100

    Added function to Question9

--- Question9.js
+++ Question9.js
@@ -1,14 +1,18 @@
-let sentence= 
-   {word: "car", length: 3,
-   word: "door", length: 4,
-   word: "type", length: 4,
-   word: "sleep", length: 5,
-   word: "carry", length: 5}
+function verifyLengthOfWord(){
 
-let sentenceArray= [sentence]
-
-for(i=0; i<sentenceArray.length; i++){
-    if(sentenceArray[i].length=== sentenceArray[i].word.length){
-        console.log(`Correct`)
-    } else (console.log(`False`))
+    let sentence= [
+       {word: "car", length: 3},
+       {word: "door", length: 4},
+       {word: "type", length: 4},
+       {word: "sleep", length: 5},
+       {word: "carry", length: 5}]
+    
+    
+    for(i=0; i<sentence.length; i++){
+        if(sentence[i].length=== sentence[i].word.length){
+            console.log(`Correct`)
+        } else (console.log(`False`))
+    }
+    return
 }
+verifyLengthOfWord()
