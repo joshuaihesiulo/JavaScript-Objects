bd385ac4d0bcf36e78dae177d2586c0d2a0ba209
Joshua Ihesiulo <joshuaihesiulo@gmai.com>
2026-03-28 09:35:06 +0100

    Added function to Question8

--- Question8.js
+++ Question8.js
@@ -1,14 +1,19 @@
-let order = {
-  customer: "Fatima",
-  items: [
-    { product: "Bag", unitPrice: 15000, qty: 2 },
-    { product: "Shoes", unitPrice: 22000, qty: 1 },
-    { product: "Belt", unitPrice: 5000, qty: 3 }
-  ]
-};
-let totalCost= 0
+function calculateTotalCost(){
 
-for(i=0; i<order.items.length; i++){
-  totalCost= totalCost + (order.items[i].unitPrice * order.items[i].qty)
+    let order = {
+      customer: "Fatima",
+      items: [
+        { product: "Bag", unitPrice: 15000, qty: 2 },
+        { product: "Shoes", unitPrice: 22000, qty: 1 },
+        { product: "Belt", unitPrice: 5000, qty: 3 }
+      ]
+    };
+    let totalCost= 0
+    
+    for(i=0; i<order.items.length; i++){
+      totalCost= totalCost + (order.items[i].unitPrice * order.items[i].qty)
+    }
+    console.log(`${order.customer}'s total cost is ₦ ${totalCost}`)
+    return
 }
-console.log(`${order.customer}'s total cost is ₦ ${totalCost}`)
+calculateTotalCost()
