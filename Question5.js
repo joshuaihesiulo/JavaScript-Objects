7d16097963a7c2755cb01a984fbce03d979703fa
Joshua Ihesiulo <joshuaihesiulo@gmai.com>
2026-03-28 09:27:07 +0100

    Added function to Question5

--- Question5.js
+++ Question5.js
@@ -1,14 +1,19 @@
-let students= [
-    {name: "Solomon", score: 95},
-    {name: "Emmanuel", score: 100},
-    {name: "George", score: 49}
-]
-let count= 0
-
-for(i=0; i<students.length; i++){
-    console.log(students[i].name, students[i].score)
-    if(students[i].score>=50){
-        count++
+function gradeScores(){
+  
+    let students= [
+        {name: "Solomon", score: 95},
+        {name: "Emmanuel", score: 100},
+        {name: "George", score: 49}
+    ]
+    let count= 0
+    
+    for(i=0; i<students.length; i++){
+        console.log(students[i].name, students[i].score)
+        if(students[i].score>=50){
+            count++
+        }
     }
+    console.log(`${count} students passed at the end.`)
+    return
 }
-console.log(`${count} students passed at the end.`)
+gradeScores()
