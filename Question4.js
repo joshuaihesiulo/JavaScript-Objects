41f721884022f2c2c41b7b2e170e0b2dc281d54b
Joshua Ihesiulo <joshuaihesiulo@gmai.com>
2026-03-28 09:25:24 +0100

    Added function to Question4

--- Question4.js
+++ Question4.js
@@ -1,12 +1,17 @@
-let products = [
-  { name: "Rice", price: 3000, quantity: 50 },
-  { name: "Beans", price: 1800, quantity: 30 },
-  { name: "Oil", price: 2500, quantity: 20 }
-];
-let totalValue= 0
-for(i=0; i<products.length; i++){
-   console.log(products[i].name, products[i].price)
+function calculateTotalPrice(){
 
-   totalValue= totalValue + (products[i].price * products[i].quantity)
-   console.log(totalValue)
+  let products = [
+    { name: "Rice", price: 3000, quantity: 50 },
+    { name: "Beans", price: 1800, quantity: 30 },
+    { name: "Oil", price: 2500, quantity: 20 }
+  ];
+  let totalValue= 0
+  for(i=0; i<products.length; i++){
+     console.log(products[i].name, products[i].price)
+  
+     totalValue= totalValue + (products[i].price * products[i].quantity)
+     console.log(totalValue)
+  }
+  return
 }
+calculateTotalPrice()
